ea676e40f359b9821f1f74d3175faf0e94806dbf
khadijaBOUKIR <kh2007.boukir@gmail.com>
2022-01-15 14:02:55 +0100

    skills.js

--- script/skills.js
+++ script/skills.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     setTimeout(() => {
-        $.getJSON('../data/languages.json', function(res) {
+        $.getJSON('../data/skills.json', function(res) {
             if (res) {
-                res['languages'].forEach(element => {
+                res['skills'].forEach(element => {
                     const childs = '<div class="n-p">' +
                         '<p class="name">' + element.name + '</p>' +
                         '<div class="percentage">' + element.percentage + '</div>' +
@@ -10,12 +10,9 @@ $(document).ready(function() {
                         '<div class="container">' +
                         `<div class="rectangle ${element.name}"></div>` +
                         '</div>';
-                    $(childs).appendTo('.languages');
+                    $(childs).appendTo('.skills');
                 });
             }
         })
     }, 3000);
-
-
-
 })
